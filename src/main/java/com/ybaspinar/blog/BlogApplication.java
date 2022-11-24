1689b6f244889f5627adf54b379fbd68018f01ea
Yusuf Başpınar <y.baspinar@yandex.com>
2022-11-25 00:58:55 +0300

    Initial commit

---
+++ src/main/java/com/ybaspinar/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.ybaspinar.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ybaspinar/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybaspinar.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
